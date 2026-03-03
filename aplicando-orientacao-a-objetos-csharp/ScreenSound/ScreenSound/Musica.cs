51fdc3ada5b530746bc341ea91fc78f59e00fd15
Jorge <workjorge19@gmail.com>
2026-03-03 16:11:56 -0300

    Criação da classe Musica

---
+++ aplicando-orientacao-a-objetos-csharp/ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao} segundos");
+        
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Disponível no plano Plus+");
+        }
+    }
+}
