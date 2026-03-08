2b8428256131e19f1083637f2be10e94ac807150
Jorge <workjorge19@gmail.com>
2026-03-08 16:32:24 -0300

    Criação de namespaces

---
+++ ScreenSound03-projeto-inicial/ScreenSound03-projeto-inicial/ScreenSound/Alura/Filmes/Filme.cs
@@ -0,0 +1,31 @@
+﻿class Filme
+{
+    public Filme(string titulo, int duracao)
+    {
+        Titulo = titulo;
+        Duracao = duracao;
+    }
+
+    public string Titulo { get; }
+    public int Duracao { get; }
+    public List<string> Elenco { get; set; }
+
+    public void AdicionarElenco(string nome)
+    {
+        Elenco.Add(nome);
+    }
+
+    public void ExibirElenco()
+    {
+        Console.WriteLine("Elenco: ");
+        foreach (var elenco in Elenco)
+        {
+            Console.WriteLine(elenco);
+        }
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Título: {Titulo}. Duração: {Duracao} min. Elenco: {string.Join(", ", Elenco)}");
+    }
+}
